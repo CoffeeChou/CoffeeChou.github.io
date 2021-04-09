98ba54db755d50759696325615d86510db6a2f28
CoffeeChou <coffee.zyr@gmail.com>
2021-04-09 15:24:43 +0800

    scroll to top with jquery
    Signed-off-by: CoffeeChou <coffee.zyr@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 $(document).ready(function() {
+    $("div.top-btn a.btn").on("click", function(){
+        window.scrollTo({top: 0, behavior: "smooth"});
+        $("html body").animate({scrollTop:0});
+        $("div.content_container").animate({scrollTop:0});
+    });
 });
